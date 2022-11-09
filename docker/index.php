bf9128c2424be8b817c6844c8662e02cb6057cdc
Tino Didriksen <mail@tinodidriksen.com>
2022-11-09 13:25:18 +0000

    Bump limit to 10k; Add duration and file size to JSON

--- docker/index.php
+++ docker/index.php
@@ -17,16 +17,16 @@ $return_type = $_REQUEST['n'] ?? 0;
 // The -V value given to lame - default to 1, but allow 1-9 inclusive
 $quality = min(max(intval($_REQUEST['q'] ?? 1), 1), 9);
 
-// If text is longer than 1024 Unicode code points, trim to nearest word under
-if (mb_strlen($text) > 1024) {
-	$text = mb_substr($text, 0, 1024);
+// If text is longer than 10000 Unicode code points, trim to nearest word under
+if (mb_strlen($text) > 10000) {
+	$text = mb_substr($text, 0, 10000);
 	$text = preg_replace('~\s*\S+$~', '', $text);
 }
 
 $hash = sha1($text).'-'.strlen($text);
 $folder = '/cache/'.substr($hash, 0, 2).'/'.substr($hash, 2, 2);
 $basename = "$folder/$hash";
-if (!file_exists("$basename.time")) {
+if (!file_exists("$basename.dur")) {
 	chdir(__DIR__);
 	if (!file_exists($folder)) {
 		shell_exec("mkdir -p $folder");
@@ -34,6 +34,7 @@ if (!file_exists("$basename.time")) {
 	$scm = '(set! utt1 (Utterance Text "'.addcslashes($text, '\\"').'")) (utt.synth utt1) (utt.save.wave utt1 "'.$basename.'.wav") (utt.save.start_times utt1 "'.$basename.'.time")';
 	$cmd = 'echo '.escapeshellarg($scm)." | /usr/bin/timeout -k 15 10 ./festival_client --port 12345 >/dev/null 2>&1";
 	shell_exec($cmd);
+	shell_exec("soxi -D '$basename.wav' > '$basename.dur'");
 }
 
 $mime = 'audio/wav';
@@ -51,6 +52,8 @@ if ($format === 'mp3') {
 if ($return_type === 'json') {
 	$json = [
 		'fn' => $rv,
+		'du' => floatval(trim(file_get_contents("$basename.dur"))),
+		'sz' => filesize("$folder/$rv"),
 		'ts' => [],
 		];
 	$ts = explode("\n", trim(file_get_contents("$basename.time")));
@@ -61,7 +64,7 @@ if ($return_type === 'json') {
 		if (!preg_match('~^([\d.]+) (.+)$~', $t, $m)) {
 			continue;
 		}
-		$json['ts'][] = [$m[1], $m[2]];
+		$json['ts'][] = [floatval($m[1]), $m[2]];
 	}
 
 	$json = json_encode($json, JSON_UNESCAPED_SLASHES|JSON_UNESCAPED_UNICODE);
